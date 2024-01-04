d992caf77068fe545b6115e6dbf7b578157b4c8f
Ilham Nur Hasanudin <ilhamadzkhan1998@gmail.com>
2024-01-04 10:31:41 +0700

    Update visibility.php

---
+++ oophp/visibility.php
@@ -0,0 +1,14 @@
+<?php 
+// visibility 
+// adalah sebuah access dari turunan sebuah class
+
+// public 
+// dapat digunakan di mana saja bahkan diluar class 
+
+// protected 
+// dapat digunakan hanya di turunannya class saja
+
+//Private
+// 
+
+?>
